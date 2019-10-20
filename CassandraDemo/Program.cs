d0b64b2fd33bb84e3d4b86c56d7d339ead60c246
Vikram shah <vshah@tavisca.com>
2019-10-20 12:00:02 +0530

    Add project files.

---
+++ CassandraDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CassandraDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
